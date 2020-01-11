2c6074b305ecc2e8221c9b77daabd5b1bf90e5be
Ted Irland <irlandth@gmail.com>
2020-01-11 09:59:05 -0600

    Defined variables, set up input values and started writing functions

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// Assignment Code
+// Assignment Code (SHOULD BE COMPLETE IN GATHERING INPUTS FROM USER)
 var generateBtn = document.querySelector("#generate");
-
+var passwordLength = prompt("How Long Would You Like Your Password To Be?")
+var specialChar = confirm ("Would you like your password to contain special characters (! @ #)?")
+var numericChar = confirm ("Would you like your password to contain numbers?");
+var lowerCaseChar = confirm ("Would you like your password to contain lowercase characters?")
+var upperCaseChar = confirm ("Would you like your password to contain UPPERCASE characters?")
 
 // Write password to the #password input
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  // passwordText.textContent = need to set a variable for passord input and use it here
 
   passwordText.value = password;
 

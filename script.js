ac1327c4f6aad248342747ec01eb96f09406ae22
tedirland <58044717+tedirland@users.noreply.github.com>
2020-01-14 22:59:40 -0600

    Add files via upload
    Final commit, with working copy function.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// DOM Elements
+// DOM Elements Established
 const resultEl = document.getElementById('password');
 const lengthEl = prompt("How many characters would you like your password to contain? (Must be between 8 and 128)");
 const uppercaseEl = confirm("Would you like your password to contain UPPERCASE characters?");
@@ -6,7 +6,6 @@ const lowercaseEl = confirm("Would you like your password to contain lowercase c
 const numbersEl = confirm("Would you like your password to contain numbers?");
 const symbolsEl = confirm("Would you like your password to contain special characters (! @ #)?");
 const generateEl = document.getElementById('generate');
-const copyEl = document.getElementById('copy');
 const clipboardEl = document.getElementById('clipboard');
 
 
@@ -20,7 +19,7 @@ var randomFunc = {
 
 };
 
-// Generate Event listen
+// Generate Event listen for Generate Button
 generateEl.addEventListener('click', () => {
   const length = Number(lengthEl);
   const hasUpper = uppercaseEl;
@@ -28,9 +27,7 @@ generateEl.addEventListener('click', () => {
   const hasNumber = numbersEl;
   const hasSymbol = symbolsEl;
 
-  console.log(typeof lengthEl);
-
-
+  //Changes Inner Text of the Result Element (TextArea) to the output of the generatePassword function
   resultEl.innerText = generatePassword(
     hasUpper,
     hasLower,
@@ -41,77 +38,69 @@ generateEl.addEventListener('click', () => {
 
 });
 
-// // Copy Password to Clipboard
+// Copy Password to Clipboard Function Defined
 
-// clipboardEl.addEventListener('click', () => {
+function copyToclip() {
 
-//   const textarea = document.createElement('textarea');
-//   const password = resultEl.innerText;
-
-//   if (!password) {
-//     return;
-//   }
-
-//   textarea.value = password;
-//   document.body.appendChild(textarea);
-//   textarea.select();
-//   document.execCommand('copy');
-//   textarea.remove();
-//   alert('Password copied to clipboard!')
+  const textarea = document.getElementById('password');
+  const password = textarea.value;
 
 
+  console.log(textarea.value);
+  textarea.select();
+  document.execCommand('copy');
+  alert("Password copied to clipboard.")
 
 
-// });
+};
+//Event Listener for copyToclip function
+clipboardEl.addEventListener('click', copyToclip);
 
-// Generate Password Function
+// Generate Password Function - passing the random number gen functions as well as the value of length from the prompt
 function generatePassword(upper, lower, number, symbol, length) {
-  // 1. Init PW Variable
-  // 2. Filter out false types
-  // 3. Loop over the length call a generator function  for each type
-  // 4. Add final pw to the pw variable and return
 
+  //Set generatedPassword to an empty string
   let generatedPassword = '';
 
+  // create a variable to track the number of different character gen functions are being run
   const typesCount = upper + lower + number + symbol;
-
-  console.log('typesCount: ', typesCount);
-
-
 
   // Creating an array to log the boolean values of the options
   const typesArr = [{ upper }, { lower }, { number }, { symbol }].filter
-    // used to weed out fales values from being returned in the array  
+    // used to weed out fales values from being returned in the array - we only want to loop through true values
     (item => Object.values(item)[0]);
+    //Isolate the boolean values
 
 
-  console.log('typesArr: ', typesArr);
-
-
-  // If no types are checked, display alert and reload the page
+  // Parameter conditionals to prevent no types being selected or a number outside of the 8-128 range
   if (typesCount === 0) {
     alert("You must select at least one type of character!");
     location.reload();
+  } else if (parseInt(lengthEl) < 8 || parseInt(lengthEl) > 128) {
+
+    alert("Passwords must be between 8 and 128 characters!")
+    location.reload();
+    
   }
 
-
-  // Throw another conditional in here to return an error if length is less than 8 and greater than 128
+  //Looping through the array, using typesCount as the incrementing factor
 
   for (let i = 0; i < length; i += typesCount) {
 
     typesArr.forEach(type => {
       const funcName = Object.keys(type)[0];
+      //Defines funcName as the specific function from the function array randomFunc
       console.log('funcName: ', funcName);
+      //Append the empty string defined by generatedPassword with the specific function that is being called by the for loop
       generatedPassword += randomFunc[funcName]();
 
     });
   }
 
-
-
+  //create new const finalPassword which slices the generatedPassword output based on the length set by user
   const finalPassword = generatedPassword.slice(0, length);
   return finalPassword;
-console.log(finalPassword);
+  console.log(finalPassword);
 }
 
 
@@ -140,38 +129,3 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 // End Generator Functions
-
-
-
-
-
-
-
-
-
-
-
-// function writePassword() {
-
-
-//       passwordText.textContent = password;
-
-  // copyBtn.removeAttribute("disabled");
-  // // copyBtn.focus();
-
-
-
-  // passwordText.textContent = need to set a variable for password input and use it here
-
-
-// }
-
-// function copyToClipboard() {
-  // BONUS 
-// }
-
-// Add event listener to generate button
-// generateBtn.addEventListener("click", generatePassword());
-// // generateBtn.addEventListener("click")
-
-// // BONUS EVENT LISTENER
